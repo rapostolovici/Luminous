0a01cf5c25e3c5f1d008df43b0033355499820e7
alexiav1 <alemvere1@gmail.com>
2022-02-15 17:10:57 +0200

    updated main
    introduced a loop where we will try to take pictures.

--- PiCamera/Test2.py
+++ PiCamera/Test2.py
@@ -1,8 +1,3 @@
 from picamera import PiCamera
 def snapshot():
-  camera = PiCamera()
-  camera.start_preview()
-  sleep(5)
-  camera.capture('home/pi/pic_1.png')
-  camera.stop_preview()
-  #camera.close()
+  
--- PiCamera/main.py
+++ PiCamera/main.py
@@ -1,1 +1,11 @@
 from Test2 import snapshot
+while 
+snapshot()
+camera = PiCamera()
+  #camera.start_preview() for monitor
+  for i in range(5):#in 3 hours the camera will take maximum 180 pics, 1 pic/min, I think we should count the 5s preview
+    sleep(5)
+    camera.capture('home/pi/image%s.jpg' % i)
+    #camera.annotate_text = "Hello world!" maybe we can use this to mention the location of the ISS when the pic was taken
+  #camera.stop_preview() preview works just for a monitor
+  camera.close()
