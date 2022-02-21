74e72de92d49fc56239456eb8589a352bbbd42a8
alexiav1 <alemvere1@gmail.com>
2022-02-21 18:07:22 +0200

    Updated main
    Changed the name of the file from where we import the snapshot function.

--- PiCamera/main.py
+++ PiCamera/main.py
@@ -1,3 +1,3 @@
-from Test2 import snapshot
-for i in range(2):
+from Test2_2 import snapshot
+for i in range(2):#we will specify the exact amount of time our program will execute, that meaning for 3 hours
   snapshot()
