e3951dc64e4a94b2e272db8a6b27437afbf505ac
alexiav1 <alemvere1@gmail.com>
2022-02-24 17:15:44 +0200

    Updated main
    Added some comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Test2_2 import snapshot
+
 from datetime import datetime, timedelta
 from logzero import logger, logfile
 from pathlib import Path
@@ -12,64 +12,70 @@ from picamera import PiCamera
 from orbit import ISS
 from skyfield.api import load
 import csv
+
+#function to convert coordinates
 def convert(angle):
+    logger.info("Displaying the coordinates")
     sign, degrees, minutes, seconds = angle.signed_dms()
     exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
     return sign < 0, exif_angle
-
+    
+#function to capture an image and add EXIF data with ISS coordinates
 def capture(camera, image):
+    logger.info("Taking a picture and finding the location of the ISS")
     """Use `camera` to capture an `image` file with lat/long EXIF data."""
     point = ISS.coordinates()
-
     # Convert the latitude and longitude to EXIF-appropriate representations
     south, exif_latitude = convert(point.latitude)
     west, exif_longitude = convert(point.longitude)
-    
-# Set the EXIF tags specifying the current location
+    # Set the EXIF tags specifying the current location
     camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
     camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
     camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
     camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
     camera.capture(image)
     return image
+    
+#function to take an image and to save it
 def snapshot(index,camera):
-  base_folder = Path(__file__).parent.resolve()#i think we can use this to identify the 'parent' folder in which we will save the images
-  #camera.start_preview() for monitor
-  #for i in range(5):#in 3 hours the camera will take maximum 180 pics, 1 pic/min, I think we should count the 5s preview
+  logger.info("Saving the image")
+  base_folder = Path(__file__).parent.resolve()
   sleep(5)
-  #camera.capture('/home/pi/image%s.jpg' % i)
   image=capture(camera, f"{base_folder}/image%s.jpg" % index)
   return image
-def clasify(image_file, data_file):
-    script_dir = Path(__file__).parent.resolve()
 
+#function to classify an image 
+def clasify(image_file,data_file):
+    script_dir = Path(__file__).parent.resolve()
     model_file = script_dir/'models/astropi-earth-water-clouds.tflite'
     data_dir = script_dir/'Data'
     label_file = data_dir/'earth-water-clouds.txt'
-    #image_file = data_dir/'tests'/'photo_04415_51846074449_o.jpg'
-interpreter = make_interpreter(f"{model_file}")
+    interpreter = make_interpreter(f"{model_file}")
     interpreter.allocate_tensors()
     size = common.input_size(interpreter)
     image = Image.open(image_file).convert('RGB').resize(size, Image.ANTIALIAS)
-
     common.set_input(interpreter, image)
     interpreter.invoke()
     classes = classify.get_classes(interpreter, top_k=1)
-labels = read_label_file(label_file)
-    
+    labels = read_label_file(label_file)
     for c in classes:
         print(f'{labels.get(c.id, c.id)} {c.score:.5f}')
         row = (datetime.now(),image_file, f'{labels.get(c.id, c.id)}', f'{c.score:.5f}')
         add_csv_data(data_file, row)
+
+#function to create a csv file
 def create_csv(data_file):
     with open(data_file, 'w') as f:
         writer = csv.writer(f)
         header = ("Date/time", "Image", "Label", "Score")
         writer.writerow(header)
+
+#function to add the data in the csv file
 def add_csv_data(data_file, data):
     with open(data_file, 'a') as f:
         writer = csv.writer(f)
         writer.writerow(data)
+        
 base_folder = Path(__file__).parent.resolve()
 data_file = base_folder/'data.csv'
 logfile(base_folder/"events.log")
@@ -83,15 +89,13 @@ running_time = 168
 index = 0
 camera = PiCamera()
 camera.resolution = (2592,1944)
-
 create_csv(data_file)
-    
+
 while (now_time < start_time + timedelta(minutes=running_time)):
   try:
     index+=1
     image = snapshot(index, camera)
-    clasify(image, data_file)
-    #sleep(1)
+    clasify(image,data_file)
     # Update the current time
     now_time = datetime.now() 
   except Exception as e:
